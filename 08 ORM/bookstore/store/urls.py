ba33a03f79ca82d2de1a8ffc81e9523add7949ac
Rajesh <rajarajesha@gmail.com>
2026-02-26 19:21:46 +0530

    'forms'

--- 08 ORM/bookstore/store/urls.py
+++ 08 ORM/bookstore/store/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('', views.book_list, name='books'),
     path('dashboard/', views.sales_dashboard, name='dashboard'),
     path('raw/', views.raw_sql_view, name='raw'),
+
+    # NEW URLS
+    path('authors/', views.author_list, name='authors'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('customers/', views.customer_orders, name='customers'),
+    path('low-stock/', views.low_stock, name='low_stock'),
+    path('customer-summary/', views.customer_summary, name='customer_summary'),
+    path('search/', views.search_books, name='search_books'),
+    path('top-books/', views.top_books, name='top_books'),
 ]
--- 08 ORM/bookstore/store/views.py
+++ 08 ORM/bookstore/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import *
 from django.db.models import Sum, F
+from django.db.models import Count
+
 from django.db import connection
 # Create your views here.
 
@@ -20,3 +22,71 @@ def raw_sql_view(request):
         
         rows = cursor.fetchall()
     return render(request, 'raw.html', {'rows':rows})
+
+
+
+def author_list(request):
+    authors = Author.objects.annotate(
+        total_books=Count('book')
+    )
+
+    return render(request, 'authors.html', {'authors': authors})
+
+def author_list(request):
+    authors = Author.objects.annotate(
+        total_books=Count('book')
+    )
+
+    return render(request, 'authors.html', {'authors': authors})
+
+def book_detail(request, book_id):
+    book = Book.objects.select_related('author').get(id=book_id)
+    orders = OrderItem.objects.filter(book=book)
+
+    context = {
+        'book': book,
+        'orders': orders
+    }
+
+    return render(request, 'book_detail.html', context)
+
+def customer_orders(request):
+    customers = Customer.objects.prefetch_related(
+        'order_set'
+    )
+
+    return render(request, 'customers.html', {'customers': customers})
+
+def low_stock(request):
+    books = Book.objects.filter(stock__lt=20).order_by('stock')
+    return render(request, 'low_stock.html', {'books': books})
+
+from django.db.models import Sum
+
+def customer_summary(request):
+    customers = Customer.objects.annotate(
+        total_items=Sum('order__orderitem__quantity')
+    )
+
+    return render(request, 'customer_summary.html', {'customers': customers})
+
+def search_books(request):
+    query = request.GET.get('q')
+
+    books = Book.objects.all()
+
+    if query:
+        books = books.filter(title__icontains=query)
+
+    return render(request, 'search.html', {
+        'books': books,
+        'query': query
+    })
+
+
+def top_books(request):
+    books = Book.objects.annotate(
+        total_sold=Sum('orderitem__quantity')
+    ).order_by('-total_sold')[:5]
+
+    return render(request, 'top_books.html', {'books': books})
